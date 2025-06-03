f499b1c858f36403ac375602a7327a7c4788d045
Robert Pratt <robert@bpong.me>
2025-06-03 15:40:30 -0500

    feat: add TeamsContext module with create_team stub function

---
+++ lib/reply_express/accounts/teams_context.ex
@@ -0,0 +1,13 @@
+defmodule ReplyExpress.Accounts.TeamsContext do
+  @moduledoc """
+  The Teams context.
+  """
+
+  @doc """
+  Creates a team with the given attributes.
+  """
+  def create_team(attrs) do
+    # TODO: Implement team creation
+    {:ok, attrs}
+  end
+end
